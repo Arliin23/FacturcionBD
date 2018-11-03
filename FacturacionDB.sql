6e8d81b80b9c6e529448c85adb5af73da71c3be3
Arianna Linette Díaz <40780642+Arliin23@users.noreply.github.com>
2018-11-03 13:25:20 -0400

    Agregada Primary Key a la tabla Usuario
    pipo

--- FacturacionDB.sql
+++ FacturacionDB.sql
@@ -87,12 +87,14 @@ FOREIGN KEY (ID_Factura) REFERENCES Factura(ID_Factura)
 );
 
 CREATE TABLE Usuario(
-Id_Usuario int Identity(80000, 1)  not null,
+Id_Usuario int Identity(80000, 1) Primary Key,
 Nombre_Usuario varchar(10) not null unique,
 Clave varchar(30) not null, 
 Estado varchar(50) not null, 
 CONSTRAINT CHK_EstadoUsuario CHECK(Estado IN('Activo', 'Vacaciones', 'Inactivo'))   
 );
+
+
 
 --Insertar valores de prueba
 insert into Cliente values ( 'Erick', '40209553250', 'Primera', 'Activo');
